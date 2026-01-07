8afedfca823d150dee54a9bf1ea771b9bb0e970a
Disha Maheshwari <disha.maheshwari@icloud.com>
2026-01-06 21:46:38 -0500

    linked main file to the quote data file instead of default data file

--- Quotely/QuotelyApp.swift
+++ Quotely/QuotelyApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct QuotelyApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Quote.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
